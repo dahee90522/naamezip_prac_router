3d24354be38e2e168b8df8b46b5c609354f63b7a
황다희 <>
2020-12-10 09:57:25 +0900

    20201210

--- src/pages/mypage/Mypage.js
+++ src/pages/mypage/Mypage.js
@@ -2,10 +2,30 @@ import React from 'react';
 import './Mypage.css';
 
 
+        // <div className="topcontainer">
+        //     <h1>{match.params.name}마이페이지임</h1>
+        // </div>
 const Mypage = ({match}) => {
     return (
-        <div className="topcontainer">
-            <h1>{match.params.name}마이페이지임</h1>
+        <div className = "mypagecontainer">
+            <div className="mypagetitle"><span>마이</span>페이지</div>
+            <div className = "mypageinfo">
+                <div className = "h3">
+                    <h3>내정보</h3>
+                    <h3>로그아웃</h3>
+                </div>
+                <div className = "mypagenamephone">{match.params.name}(19, 여) / 01023191342<span>수정</span></div>
+                <div className = "mypageemail">itsdahee@kakao.com<span>카카오계정</span></div>
+            </div>
+            <div className = "mypagecardinfo">
+                <div className = "h3">
+                    <h3>등록카드</h3>
+                    <h3>수정</h3>
+                </div>
+                <div className = "cardtrue">카드가 등록되어 있습니다.</div>
+            </div>
+            <div className="mypagenowtitle"><span>남의집</span>현황</div>
+            
         </div>
     );
 };
