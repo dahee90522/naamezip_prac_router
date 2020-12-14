383f803d95906fb9b8f2f2cf7ccc1e0ed54a78cb
dahee90522 <>
2020-12-14 16:36:01 +0900

    20201214

--- src/shared/auth/Logoutbtn.js
+++ src/shared/auth/Logoutbtn.js
@@ -6,7 +6,7 @@ import { withRouter } from 'react-router-dom';
 function Logoutbtn({ logout, history }) {
 	const handleClick = () => {
 		logout();
-		history.push('/');
+		history.push('/'); 
 	};
 	return <h3 onClick={handleClick}>로그아웃</h3>;
 
